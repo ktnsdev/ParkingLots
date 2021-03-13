9d62895f01fc21437b1a8dc83ce8604df7502408
ktns <pom-2551@hotmail.com>
2021-03-13 22:26:17 +0700

    Added tables and minute-date-hour selector to the contribution page

--- components/widgets/contribution/ContributeParkingLotFee.js
+++ components/widgets/contribution/ContributeParkingLotFee.js
@@ -1,21 +1,115 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import GrayTextInputWithTitle from '../GrayTextInputWIthTitle';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import GrayTextInputWithTitle, { output } from '../GrayTextInputWIthTitle';
 import TextWithFont from '../TextWithFont';
 import ParkingFeeConditionAdder from './ParkingFeeConditionAdder';
+import HourMinuteDayButton from '../HourMinuteDayButton';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const ContributeParkingLotFee = () => {
+    const [unitTime, setUnitTime] = useState('hour');
+    const [time, setTime] = useState(0);
+    const [charIsValid, setCharIsValid] = useState(true);
+    const [timeIsValid, setTimeIsValid] = useState(true);
+    const [inputNotEmpty, setInputNotEmpty] = useState(false);
+
+    function handleTextInputOutput(output) {
+        if (output == '' || output == undefined) {
+            setTime(0);
+            setCharIsValid(true);
+            setTimeIsValid(true);
+            setInputNotEmpty(false);
+            return;
+        }
+
+        if (output.indexOf('.') > -1) {
+            setTime(0);
+            setCharIsValid(false);
+            setTimeIsValid(true);
+            setInputNotEmpty(true);
+            return;
+        }
+
+        if (!isInt(output)) {
+            setTime(0);
+            setCharIsValid(false);
+            setTimeIsValid(true);
+            setInputNotEmpty(true);
+        } else {
+            setTime(parseInt(output));
+            setCharIsValid(true);
+            setInputNotEmpty(true);
+            
+            if (unitTime == 'hour' && (parseInt(output) > 23 || parseInt(output) < 0)) {
+                setTimeIsValid(false);
+            } else if (unitTime == 'minute' && (parseInt(output) > 59 || parseInt(output) < 0)) {
+                setTimeIsValid(false);
+            } else if (unitTime == 'day' && (parseInt(output) > 30 || parseInt(output) < 0)) {
+                setTimeIsValid(false);
+            } else {
+                setTimeIsValid(true);
+            }
+        }
+    }
+
+    const getUnitTime = (data) => {
+        setUnitTime(data);
+    }
+
+    function isInt(str) {
+        return !isNaN(str) && Number.isInteger(parseFloat(str));
+      }
+
+    function renderParkingFeeConditionAdder() {
+        return (
+            <>
+                <View style={{ marginTop: '2%' }}>
+                    <ParkingFeeConditionAdder
+                        title={'How are the parking fees calculated?'}
+                        subtitle={'Provide the information about the parking fees below.'}
+                        firstFreeTime={time}
+                        firstUnitTime={unitTime}
+                    />
+                </View>
+
+                <View style={styles.lineBreak}></View>
+            </>
+        )
+    }
+
     return (
         <>
-            <GrayTextInputWithTitle title={'How many hours are free of charge?'} subtitle={'eg. Type "2" if the first 2 hours are free,\nor type "0" if the place doesn\'t offer you any free hours.'} titleFontSize={18} placeholder={'Required, must be a number'} />
-            <ParkingFeeConditionAdder
-                title={'How the parking fee are calculated?'}
-                subtitle={'Provide the information about the parking fee below.'}
-            />
+            <View style={{ marginBottom: '2%' }}>
+                <GrayTextInputWithTitle
+                    title={'How many ' + unitTime + 's are free of charge?'}
+                    subtitle={'Enter 0 if the place doesn\'t offer you any free hours.'}
+                    titleFontSize={18}
+                    placeholder={'Required, must be a number'}
+                    keyboardType={'number-pad'}
+                    onChangeText={input => handleTextInputOutput(input)}
+                    returnKeyType='done'
+                    charIsValid={charIsValid}
+                    timeIsValid={timeIsValid}
+                    unitTime={unitTime}
+                />
+
+                <HourMinuteDayButton initial={'hr'} callbackFunction={getUnitTime}/>
+            </View>
+
+            <View style={styles.lineBreak}></View>
+
+            {(inputNotEmpty && charIsValid && timeIsValid) && renderParkingFeeConditionAdder()}
         </>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    lineBreak: {
+        borderBottomColor: '#404040',
+        borderBottomWidth: 0.5,
+        marginTop: "3%",
+        marginBottom: "3%"
+    }
+});
 
 export default ContributeParkingLotFee
