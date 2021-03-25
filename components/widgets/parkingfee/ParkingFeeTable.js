f9e95760a6ce96a0f1fc54233c66ff25c39e4ca1
ktns <pom-2551@hotmail.com>
2021-03-25 15:51:51 +0700

    Initial commit

---
+++ components/widgets/parkingfee/ParkingFeeTable.js
@@ -0,0 +1,160 @@
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, View, Platform, TextInputComponent, FlatList } from 'react-native';
+import TextWithFont from '../TextWithFont';
+
+const ParkingFeeTable = (props) => {
+    const DAY_IN_MINUTE = 1440;
+    const HOUR_IN_MINUTE = 60;
+
+    const [priceArray, setPriceArray] = useState([]);
+    const [unitTime, setUnitTime] = useState('default');
+    const [firstFreeTime, setFirstFreeTime] = useState({'day': 0, 'hour': 0, 'minute': 0});
+
+    useEffect(() => {
+        reformatPriceArray();
+        reformatFirstFreeTime();
+    }, [])
+
+    function reformatPriceArray() {
+        let tempPriceArray = [];
+
+        Object.keys(props.price.after_free).map((time, fee) => {
+            var d = Math.floor(time / 1440);
+            var h = Math.floor((time - (d * 1440)) / 60);
+            var m = Math.round(time % 60);
+
+            tempPriceArray.push({ 'time': {'day': d, 'hour': h, 'minute': m}, 'fee': fee });
+        })
+        setPriceArray(tempPriceArray);
+    }
+
+    function reformatFirstFreeTime() {
+        if (props.price.first_free != 0) {
+            var d = Math.floor(props.price.first_free / 1440);
+            var h = Math.floor((props.price.first_free - (d * 1440)) / 60);
+            var m = Math.round(props.price.first_free % 60);
+    
+            setFirstFreeTime({'day': d, 'hour': h, 'minute': m});
+        }
+    }
+
+    function renderHeader() {
+        return (
+            <>
+                <View style={{
+                    flexDirection: 'row',
+                    marginTop: 3,
+                    marginBottom: 10
+                }}>
+                    <View style={{ width: '49.9%', alignItems: 'center' }}>
+                        <TextWithFont fontSize={16} androidFontWeight={'bold'} iosFontWeight={'600'}>Parking Time</TextWithFont>
+                    </View>
+
+                    <View style={{ backgroundColor: '#666', width: '0.2%', marginTop: '-1%', marginBottom: '-3%' }} />
+
+                    <View style={{ width: '49.9%', alignItems: 'center' }}>
+                        <TextWithFont fontSize={16} androidFontWeight={'bold'} iosFontWeight={'600'}>Fee</TextWithFont>
+                    </View>
+                </View>
+                <View style={{ backgroundColor: '#666', height: 0.7, width: '100%', marginLeft: 3, marginRight: 3, marginVertical: 0 }} />
+            </>
+        )
+    }
+
+    function renderFirstFreeTime() {
+        if (props.price.first_free != 0) {
+            return (
+                <>
+                    <View style={{
+                        flexDirection: 'row',
+                        marginVertical: 5
+                    }}>
+                        <View style={{ width: '49.9%', alignItems: 'center' }}>
+                            {props.price.first_free != 0 &&
+                                <>
+                                    <View style={{ marginVertical: 6 }}>
+                                        <TextWithFont fontSize={16} androidFontWeight={'bold'} iosFontWeight={'600'}>
+                                            First {firstFreeTime.day == 0 ? '' : '' + firstFreeTime.day + ' day'}{(firstFreeTime.day != 0 && firstFreeTime.day != 1) ? 's' : '' + (firstFreeTime.hour != 0 || firstFreeTime.minute != 0) && firstFreeTime.day != 0 ? ' ' : ''}
+                                            {firstFreeTime.hour == 0 ? '' : '' + firstFreeTime.hour + ' hour'}{(firstFreeTime.hour != 0 && firstFreeTime.hour != 1) ? 's' : '' + (firstFreeTime.minute != 0) && firstFreeTime.hour != 0 ? ' ' : ''}
+                                            {firstFreeTime.minute == 0 ? '' : '' + firstFreeTime.minute + ' minute'}{(firstFreeTime.minute != 0 && firstFreeTime.minute != 1) ? 's' : ''}
+                                        </TextWithFont>
+                                    </View>
+                                </>
+                            }
+                        </View>
+
+                        <View style={{ backgroundColor: '#666', width: '0.2%', marginVertical: -5 }} />
+
+                        <View style={{ width: '49.9%', alignItems: 'center' }}>
+                            {props.firstFreeTime != 0 &&
+                                <>
+                                    <View style={{ marginVertical: 6 }}>
+                                        <TextWithFont {...props} fontSize={16} androidFontWeight={'bold'} iosFontWeight={'600'}>Free</TextWithFont>
+                                    </View>
+                                </>
+                            }
+                        </View>
+                    </View>
+                </>
+            )
+        }
+    }
+
+    function renderConditions() {
+        return (
+            <FlatList
+                style={{ width: '100%' }}
+                scrollEnabled={false}
+                data={priceArray}
+                renderItem={({ item }) => (
+                    <>
+                        {(!props.price.firstFreeTime != 0) && <View style={{ backgroundColor: '#666', height: 0.7, width: '100%', marginLeft: 3, marginRight: 3, marginVertical: 0 }} />}
+                        <View style={{
+                            flexDirection: 'row',
+                            marginVertical: 5
+                        }}>
+                            <View style={{ width: '49.9%', alignItems: 'center' }}>
+                                <View style={{ marginVertical: 6, alignItems: 'center' }}>
+                                    {item == priceArray[priceArray.length - 1] && <TextWithFont fontSize={16} androidFontWeight={'bold'} iosFontWeight={'600'}>After {props.firstUnitTime == 'day' ? 'day' : 'hour'} {item.time}</TextWithFont>}
+                                    {item != priceArray[priceArray.length - 1] && <TextWithFont fontSize={16} androidFontWeight={'bold'} iosFontWeight={'600'}>{props.firstUnitTime == 'day' ? 'Day' : 'Hour'} {item.time}</TextWithFont>}
+                                </View>
+                            </View>
+
+                            <View style={{ backgroundColor: '#666', width: '0.2%', marginVertical: -5 }} />
+
+                            <View style={{ width: '49.9%', alignItems: 'center', flexDirection: 'row', justifyContent: 'center' }}>
+                                <TextWithFont iosFontWeight={'600'} androidFontWeight={'bold'}>{item.fee} THB</TextWithFont>
+                            </View>
+                        </View>
+                    </>
+                )}
+            />
+        )
+    }
+
+    return (
+        <>
+            <View style={{
+                marginTop: '2%',
+                paddingVertical: '3%',
+                paddingHorizontal: '4%',
+                backgroundColor: '#f5f5f5',
+                borderRadius: 12,
+                shadowColor: "#333",
+                shadowOffset: {
+                    width: 0,
+                    height: 0,
+                },
+                shadowOpacity: 0.29,
+                shadowRadius: 2,
+                elevation: 7,
+            }}>
+                {renderHeader()}
+                {renderFirstFreeTime()}
+                {renderConditions()}
+            </View>
+        </>
+    )
+}
+
+export default ParkingFeeTable;
