792ec12915018c97b67ef4fa6aeb0ef8e7507348
ktns <pom-2551@hotmail.com>
2021-03-26 13:40:55 +0700

    Renamed the parking lot details page

--- components/pages/ParkingLotDetails.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ParkingLotDetails = ({ route, navigation }) {
-    
-}
-
-export ParkingLotDetails;
---
+++ components/pages/ParkingLotDetailsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ParkingLotDetailsPage = ({ route, navigation }) {
+    
+}
+
+export ParkingLotDetailsPage;
